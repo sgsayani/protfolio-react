153069c61f6085fa80873f6acfbccfa6d098ac04
sgsayani <sgsayanighatak@gmail.com>
2025-10-19 01:10:20 +0530

    3rd commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/SG.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div id='home' className='hero'>
             <div className='hero-profile-img'>
                 <img src={profile_img} alt="" />
             </div>
             <h1><span>I'm Sayani Ghatak, </span>frontend developer at TCS</h1>
             <p>I am a frontend developer with a passion for creating user-friendly and visually appealing websites. I have experience in designing and developing responsive websites, as well as front-end development using HTML, CSS, ReactJS, AngularJs, NextJs and JavaScript. I am constantly learning and improving my skills to stay at the forefront of web development.</p>
             <div className="hero-action">
-                <div className="hero-connect">
-                    Connect with me
+                <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>
+                    Connect with me</AnchorLink>
                 </div>
                 <div className="hero-resume">
                     My resume
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logos.svg'
+import underline from '../../assets/nav_underline.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 
 const Navbar = () => {
+
+  const[menu,setMenu] =useState("home");
   return (
     <div className='navbar'>
       <div className='logo'>
         <img src={logo} alt="" />
       </div>
       <ul className='nav-menu'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Protfolio</li>
-        <li>Contact</li>
+        <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu === "home"? <img src={underline} alt=''/>: <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About</p></AnchorLink>{menu === "about"? <img src={underline} alt=''/>: <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu === "services"? <img src={underline} alt=''/>: <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={()=>setMenu("work")}>Protfolio</p></AnchorLink>{menu === "work"? <img src={underline} alt=''/>: <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact"? <img src={underline} alt=''/>: <></>}</li>
       </ul>
-      <div className='nav-connect'>Connect With me</div>
+      <div className='nav-connect'><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With me</AnchorLink></div>
 
     </div>
   )
